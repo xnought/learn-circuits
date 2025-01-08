7c7f81ab41a492330c79f908a7eb2aafec45e642
xnought <drbertucci@hotmail.com>
2025-01-07 22:55:18 -0500

    cleanup

--- full-adder/main.v
+++ full-adder/main.v
@@ -1,28 +1,28 @@
 
-module half_adder(input wire a, input wire b, output wire s, output wire c);
-	assign s = a^b;
-	assign c = a&b;
+module half_adder(input wire a, input wire b, output wire sum, output wire carry);
+	assign sum = a^b;
+	assign carry = a&b;
 endmodule
 
 // https://i.sstatic.net/E0itr.png
-module full_adder(input wire a, input wire b, input wire i, output wire s, output wire c);
-	wire s0;
-	wire cout[1:0];
-	half_adder ha0 ( // a+b -> s0, c0
+module full_adder(input wire a, input wire b, input wire in_carry, output wire sum, output wire out_carry);
+	wire first_add;
+	wire half_adder_carries[1:0];
+	half_adder ha0 ( 
 		.a(a),
 		.b(b),
-		.s(s0),
-		.c(cout[0])
+		.sum(first_add),
+		.carry(half_adder_carries[0])
 	);
 
-	half_adder ha1 ( // s0+i -> s, c1
-		.a(s0),
-		.b(i),
-		.s(s),
-		.c(cout[1])
+	half_adder ha1 (
+		.a(first_add),
+		.b(in_carry),
+		.sum(sum),
+		.carry(half_adder_carries[1])
 	);
 
-	assign c = cout[0]|cout[1]; // c0 or c1 -> c
+	assign out_carry = half_adder_carries[0]|half_adder_carries[1];
 endmodule
 
 module main;
@@ -36,9 +36,9 @@ module main;
 	full_adder uut (
 		.a(a),
 		.b(b),
-		.i(i),
-		.s(s),
-		.c(c)
+		.in_carry(i),
+		.sum(s),
+		.out_carry(c)
 	);
 
 	initial begin
