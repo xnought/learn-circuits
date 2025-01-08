eda9f0d44269ef77cec2457c118807b7ef2e88e6
xnought <drbertucci@hotmail.com>
2025-01-07 22:20:59 -0500

    in and out

--- full-adder/main.v
+++ full-adder/main.v
@@ -4,9 +4,9 @@ module half_adder(input wire a, input wire b, output wire s, output wire c);
 	assign c = a&b;
 endmodule
 
-// module full_adder(input wire a, input wire b, input wire i, output wire s, output wire c)
-	
-// endmodule
+module full_adder(input wire a, input wire b, input wire i, output wire s, output wire c);
+
+endmodule
 
 module main;
  	reg a;
@@ -14,23 +14,24 @@ module main;
 	reg i;
 	wire s; // sum
 	wire c; // carry
+	integer x,y,z;
 
 	full_adder uut (
 		.a(a),
 		.b(b),
-		.i(i)
+		.i(i),
 		.s(s),
 		.c(c)
 	);
 
 	initial begin
-        $monitor("Time: %0d, a: %b, b: %b, inCarry: %b, s: %b, outCarry: %b", $time, a, b, i, s, c);
-        for (i = 0; i <= 1; i = i + 1) begin
-            for (j = 0; j <= 1; j = j + 1) begin
-            	for (k = 0; k <= 1; k = k + 1) begin
-					a = i; 
-					b = j; 
-					i = k;
+        $monitor("Time: %0d, a: %b, b: %b, inCarry: %b\nOUTPUTS: s: %b, outCarry: %b\n", $time, a, b, i, s, c);
+        for (x = 0; x <= 1; x = x + 1) begin
+            for (y = 0; y <= 1; y = y + 1) begin
+            	for (z = 0; z <= 1; z = z + 1) begin
+					a = x; 
+					b = y; 
+					i = z;
 					#10;   // Wait for 10 time units
 				end
             end
